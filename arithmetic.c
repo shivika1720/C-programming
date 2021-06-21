779a9b78ce213845f1cfc713aa0c35822e5f86f0
Shivika Kejriwal <shivika.kejriwal2000@gmail.com>
2021-06-21 23:28:14 +0530

    basic c

---
+++ arithmetic.c
@@ -0,0 +1,10 @@
+main()
+{
+int x,y;
+printf("enter two numbers ");
+scanf("%d %d \n",&x,&y);
+printf("sum is= %d \n",x+y);
+printf("difference is= %d \n",x-y);
+printf("reminder is= %d \n",x%y);
+printf("division is = %d \n",x/y);
+}
